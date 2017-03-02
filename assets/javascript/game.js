a221ff30eba5fe8203ea1fc4cccfd77b157e18b3
mkearney@google.com <mkearney@google.com>
2017-03-01 20:40:13 -0800

    showSection bug fixing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,191 @@
+
+// Data
+//_____________________________________________________________________________________
+// Create array of questions.
+// Each question is an object with a question, array of answers, and correct answer.
+// Each question also has a youtube video id.
+
+var questions = {
+	q1 : {
+		question: "Testing question 1",
+		answers: ["Testing answer 1.1", "Testing answer 1.2", "Testing answer 1.3"],
+		correctAnswer: "questions.q1.answers[0]",
+		src: "https://www.youtube.com/embed/-gOMfsWefVA?list=PLysEUMhoon6cA16AhpYW96KvlRgEXBUE9"
+	},
+	q2 : {
+		question: "Testing question 2",
+		answers: ["Testing answer 2.1", "Testing answer 2.2", "Testing answer 2.3"],
+		correctAnswer: "questions.q2.answers[0]",
+		src: "https://www.youtube.com/embed/-gOMfsWefVA?list=PLysEUMhoon6cA16AhpYW96KvlRgEXBUE9"
+	},
+	q3 : {
+		question: "Testing question 3",
+		answers: ["Testing answer 3.1", "Testing answer 3.2", "Testing answer 3.3"],
+		correctAnswer: "questions.q3.answers[0]",
+		src: "https://www.youtube.com/embed/-gOMfsWefVA?list=PLysEUMhoon6cA16AhpYW96KvlRgEXBUE9"
+	},
+	q4 : {
+		question: "Testing question 4",
+		answers: ["Testing answer 4.1", "Testing answer 4.2", "Testing answer 4.3"],
+		correctAnswer: "questions.q4.answers[0]",
+		src: "https://www.youtube.com/embed/-gOMfsWefVA?list=PLysEUMhoon6cA16AhpYW96KvlRgEXBUE9"
+	},
+	q5 : {
+		question: "Testing question 5",
+		answers: ["Testing answer 5.1", "Testing answer 5.2", "Testing answer 5.3"],
+		correctAnswer: "questions.q5.answers[0]",
+		src: "https://www.youtube.com/embed/-gOMfsWefVA?list=PLysEUMhoon6cA16AhpYW96KvlRgEXBUE9"
+	}
+};
+
+
+var questionsArray = [questions.q1, questions.q2, questions.q3, questions.q4, questions.q5];
+
+// Control which page is displayed.
+var startPage = $("#start-page");
+var questionPage = $("#question-page");
+var answerPage = $("#answer-page");
+var resultsPage = $("#results-page");
+
+// Variables.
+//____________________________________________________________________________________
+var ranOutofTime = false;
+var questionID;
+// answerID;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+var section = "";
+
+// Event listeners.
+//____________________________________________________________________________________
+
+// Fires when start button clicked.
+function addStartClickListener() {
+	$("#start").on("click", function() {
+		console.log("Start button clicked");
+		showSection(questionPage);
+	});
+};
+
+// Fires when answer list item selected.
+function addAnswerClickListener() {
+	$("#answer").on("click", function() {
+		console.log("Answer list item clicked");
+		showSection(answerPage);
+		// Set question ID to question page ID.
+		// Set answer ID to the selected answer.
+		// Calls showSection passing in question ID and answer ID.
+	});
+};
+
+// Fires when start over? button clicked.
+function addRestartClickListener() {
+	$("#restart").on("click", function() {
+		console.log("Start Over? button clicked");
+		showSection(startPage);
+		// This is essentially the same as 'start' button at start of game.
+		// But given ids are once-off, and maybe there are some differences,
+		// So I've created this listener too.
+		// Before calling showSection, set question ID to first question.
+		// Calls showSection passing in question Section and question ID.
+	});
+};
+
+// Functions
+//_____________________________________________________________________________________
+
+
+// Shows active section and hides in-active sections.
+// Takes parameter sectionId. Might need to take in answerID,
+// And correctAnswers, incorrectAnswers, and unanswered too, if I can't keep the counts local.
+// Start section is the default active section. 
+// We don't have to do anything for it, other than make it active, as it displays everything in it,
+// and nothing else.
+// For question Section, call createQuestionSection, passing in questionID.
+// For answer Section, call createAnswerSection, passing in questionID and answerID.
+// For results Section, call createResultsSection, passing in correctAnswers, incorrectAnswers, unanswered.
+
+function showSection(section) {
+	startPage.css({'display' : 'none'});
+	questionPage.css({'display' : 'none'});
+	answerPage.css({'display' : 'none'});
+	resultsPage.css({'display' : 'none'});
+
+	if (section) {
+		section.css({'display' : 'block'});
+	}
+};
+
+// Function: createQuestionSection
+// Calls displayQuestion passing in question object.
+// Calls startTimer passing in question countdown amount.
+// Calls displayTimer.
+// If startTimer returns true (which means timer reached zero),
+// Set answerID to null or something like that
+// And call showSection passing in answer Section with question ID and null answer value.
+
+// Function: createAnswerSection
+// Call stopTimer.
+// If answer is correct,
+// correctAnsers++
+// Display answer correct message in answer-assessment.
+// Don't display correct answer info.
+// Else if answer not correct,
+// incorrectAnswers++,
+// Display answer wrong message in answer-assessment.
+// Display correct answer info.
+// else answer is null (or unanswered)
+// unanswered++,
+// Display in answer-assessment the time ran out message.
+// Display correct answer info.
+// For all answer results, call displayVideo with question ID video ID.
+// Calls startTimer passing in answer countdown amount.
+// When startTimer returns,
+// If question ID ++ exists (so there's still more questions to ask),
+// Call showSection for question section, passing in question ++ as question ID.
+// else call showSection for results section passing in unanswered, answered, and incorrect.
+// I might need to set these values here, to make sure they pass accordingly.
+// It may be a little tricky to save values for these variables as questions get answered.
+
+// Function: createResultsSection
+// Call stopTimer.
+// Display correct answer count in correct-answers span.
+// Display incorrect answers count in incorrect-answers span.
+// Display unanswered count in unanswered span.
+
+// Function: displayQuestion
+// Takes parameter of question object.
+// Displays question in #question.
+// Displays list of answers for question in #answer-choices.
+
+// Function: startTimer
+// Takes in parameter of timer amount.
+// Starts timer countdown from this amount.
+// If time reaches zero,
+// return ranOutOfTime boolean to true.
+
+// Function: stopTimer
+// If ranOutofTime is true, or 
+// Displays stopped time value in the timer span on answer Section.
+
+// Function: displayVideo
+// Takes in question ID and question's video ID as parameters.
+// Displays and automatically displays video in answer-media section.
+
 $(document).ready(function() {
-
-	// Data
-	// Create question object with each question containing an array of answers.
-	// Each question also has a youtube video id.
-
-	// Variables.
-	// ranOutofTime boolean;
-	// unansweredQuestion boolean;
-	// questionID;
-	// answerID;
-	// correctAnswers = 0;
-	// incorrectAnswers = 0;
-	// unanswered = 0;
-
-	// Event listeners.
-
-	// Implement event listener for start button:
-	// Before calling showSection, set question ID to first question.
-	// Calls showSection passing in question Section and question ID.
-
-	// Implement event listener for click on answer:
-	// Set question ID to question page ID.
-	// Set answer ID to the selected answer.
-	// Calls showSection passing in question ID and answer ID.
-
-	// Implement event listener for restart button:
-	// This is essentially the same as 'start' button at start of game.
-	// But given ids are once-off, and maybe there are some differences,
-	// So I've created this listener too.
-	// Before calling showSection, set question ID to first question.
-	// Calls showSection passing in question Section and question ID.
-
-	// Functions.
-
-	// Function: showSection
-	// Shows active section and hides in-active sections.
-	// Takes parameter sectionId and question ID. Might need to take in answerID,
-	// And correctAnswers, incorrectAnswers, and unanswered too, if I can't keep the counts local.
-	// Start section is the default active section. 
-	// We don't have to do anything for it, other than make it active, as it displays everything in it,
-	// and nothing else.
-	// For question Section, call createQuestionSection, passing in questionID.
-	// For answer Section, call createAnswerSection, passing in questionID and answerID.
-	// For results Section, call createResultsSection, passing in correctAnswers, incorrectAnswers, unanswered.
-
-	// Function: createQuestionSection
-	// Calls displayQuestion passing in question object.
-	// Calls startTimer passing in question countdown amount.
-	// Calls displayTimer.
-	// If startTimer returns true (which means timer reached zero),
-	// Set answerID to null or something like that
-	// And call showSection passing in answer Section with question ID and null answer value.
-
-	// Function: createAnswerSection
-	// Call stopTimer.
-	// If answer is correct,
-	// correctAnsers++
-	// Display answer correct message in answer-assessment.
-	// Don't display correct answer info.
-	// Else if answer not correct,
-	// incorrectAnswers++,
-	// Display answer wrong message in answer-assessment.
-	// Display correct answer info.
-	// else answer is null (or unanswered)
-	// unanswered++,
-	// Display in answer-assessment the time ran out message.
-	// Display correct answer info.
-	// For all answer results, call displayVideo with question ID video ID.
-	// Calls startTimer passing in answer countdown amount.
-	// When startTimer returns,
-	// If question ID ++ exists (so there's still more questions to ask),
-	// Call showSection for question section, passing in question ++ as question ID.
-	// else call showSection for results section passing in unanswered, answered, and incorrect.
-	// I might need to set these values here, to make sure they pass accordingly.
-	// It may be a little tricky to save values for these variables as questions get answered.
-
-	// Function: createResultsSection
-	// Call stopTimer.
-	// Display correct answer count in correct-answers span.
-	// Display incorrect answers count in incorrect-answers span.
-	// Display unanswered count in unanswered span.
-
-	// Function: displayQuestion
-	// Takes parameter of question object.
-	// Displays question in #question.
-	// Displays list of answers for question in #answer-choices.
-
-	// Function: startTimer
-	// Takes in parameter of timer amount.
-	// Starts timer countdown from this amount.
-	// If time reaches zero,
-	// return ranOutOfTime boolean to true.
+	console.log(questionsArray[0]);
 	
-	// Function: stopTimer
-	// If ranOutofTime is true, or 
-	// Displays stopped time value in the timer span on answer Section.
+	// Displays start page.
+	showSection(startPage);
 
-	// Function: displayVideo
-	// Takes in question ID and question's video ID as parameters.
-	// Displays and automatically displays video in answer-media section.
+	// Listens for stark click event.
+	addStartClickListener();
+	
+	// Listens for answer click event.
+	addAnswerClickListener();
+	
+	// Listens for restart click event
+	addRestartClickListener();
+
 });
